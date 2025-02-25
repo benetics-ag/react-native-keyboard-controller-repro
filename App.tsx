05037797cbde954f0d4dee016527a01d6b7a52cf
Johan Tibell <johan.tibell@gmail.com>
2025-02-25 16:07:30 +0100

    Introduce react-navigation and react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,29 +1,51 @@
 import React from 'react';
+import {useHeaderHeight} from '@react-navigation/elements';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {
   Button,
+  Platform,
   StatusBar,
   StyleSheet,
   TextInput,
   View,
 } from 'react-native';
-import { KeyboardAvoidingView, KeyboardProvider } from 'react-native-keyboard-controller';
-import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+import {
+  KeyboardAvoidingView,
+  KeyboardProvider,
+} from 'react-native-keyboard-controller';
+import {Appbar} from 'react-native-paper';
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
+const Stack = createNativeStackNavigator<{Example: undefined}>();
 
-function Example (): React.JSX.Element {
-  const statusBarHeight = StatusBar.currentHeight ?? 0;  // Always 0 on iOS
-  const {top} = useSafeAreaInsets();  // 59 on iOS
+function Example(): React.JSX.Element {
+  const statusBarHeight = StatusBar.currentHeight ?? 0; // Always 0 on iOS
+  const headerHeight = useHeaderHeight();
+  const {top} = useSafeAreaInsets();
+  console.debug({top, statusBarHeight, headerHeight, os: Platform.OS});
+  // iOS:
+  //     {"headerHeight": 211, "os": "ios", "statusBarHeight": 0, "top": 59}
+  //     211 - 59 = 152, which is the expected AppbarHeader height.
+  // Android:
+  //     {"headerHeight": 152, "os": "android", "statusBarHeight":
+  //     54.095237731933594, "top": 0}
+  //     152 is the expected AppbarHeader height.
 
   return (
     <SafeAreaView
-      edges={['bottom', 'left', 'right', 'top']}
+      edges={['bottom', 'left', 'right']}
       style={styles.safeContainer}>
       <KeyboardAvoidingView
         behavior="height"
-        keyboardVerticalOffset={0}
+        keyboardVerticalOffset={headerHeight + statusBarHeight}
         style={styles.content}>
         <View style={styles.inner}>
-          <TextInput placeholder="Name" style={styles.textInput}  />
+          <TextInput placeholder="Name" style={styles.textInput} />
           <Button title="Submit" />
         </View>
       </KeyboardAvoidingView>
@@ -31,12 +53,25 @@ function Example (): React.JSX.Element {
   );
 }
 
+// Should be 152 dp high:
+// https://callstack.github.io/react-native-paper/docs/components/Appbar/
+function AppbarHeader(): React.JSX.Element {
+  return (
+    <Appbar.Header mode="large">
+      <Appbar.Content title="Title" />
+    </Appbar.Header>
+  );
+}
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
       <KeyboardProvider>
-        <Example />
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{header: AppbarHeader}}>
+            <Stack.Screen name="Example" component={Example} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </KeyboardProvider>
     </SafeAreaProvider>
   );
@@ -50,7 +85,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   content: {
-    flex:1,
+    flex: 1,
   },
   container: {
     flex: 1,
